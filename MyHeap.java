4bc942a0ce5d78a24e7b9d1113c754f4af5826df
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-04-29 16:55:37 -0400

    CS Dojo

--- MyHeap.java
+++ MyHeap.java
@@ -2,12 +2,22 @@ public class MyHeap{
     private static void pushDown(int[]data,int size,int index){
         int parent = data[index];
         boolean done = false;
-        while (!done){
-            if (data[index] > data[index * 2 + 1] && data[index * 2 + 2]){
+        while (!done || index * 2 + 1 > size){
+
+            // System.out.println("lljkdkahDKHSDKHA");
+            if (index * 2 + 2 >= size){
+                if (data[index] > data[index * 2 + 1]){
+                    done = true;
+                }
+                int hold = data [index];
+                data [index] = data [index * 2 + 1];
+                data [index * 2 + 1] = hold;
+            }
+            else if (data[index] > data[index * 2 + 1] && data[index] > data[index * 2 + 2]){
+                // System.out.println("pip");
                 done = true;
             }
-            else{
-                if (data[index * 2 + 1] > data[index * 2 + 2]){
+            else if (data[index * 2 + 1] > data[index * 2 + 2]){
                     int hold = data [index];
                     data [index] = data [index * 2 + 1];
                     data [index * 2 + 1] = hold;
@@ -17,8 +27,8 @@ public class MyHeap{
                     data [index] = data [index * 2 + 2];
                     data [index * 2 + 2] = hold;
                 }
-            }
         }
+        // System.out.println("loolololololo");
     }
          // - size  is the number of elements in the data array.
          // - push the element at index i downward into the correct position. This will swap with the larger of the child nodes provided thatchild is larger. This stops when a leaf is reached, or neither child is larger. [ should be O(logn) ]
@@ -49,25 +59,38 @@ public class MyHeap{
 
     //We will discuss this today:
     public static void heapify(int[] a){
-        int end = a.length - 1;
-        int max = a[end];
-        int pos = end;
-        for (int y = 0; y < a.length - 1; y ++){
-            for (int x = 0; x <= end; x ++){
-                if (a[x] > max){
-                    max = a [x];
-                    pos = x;
-                }
+        // System.out.println("kkkkkk");
+        int index = a.length - 1;
+        while (index >= 0){
+            if(2 * index + 2 > a.length - 1 && 2 * index + 1 > a.length - 1){
+                index--;
             }
-                a [pos] = a [end];
-                a [end] = max;
-                end --;
+            else{
+                pushDown(a, a.length, index);
+                index --;
             }
+        }
+
     }
         // - convert the array into a valid heap. [ should be O(n) ]
 
     public static void heapsort(int[] a){
-        ;
+        heapify(a);
+
+        int size = a.length;
+        while (size > 0){
+            // System.out.println("help" + size);
+            int hold = a[0];
+            a[0] = a[size - 1];
+            a[size - 1] = hold;
+            pushDown(a, size, 0);
+            size --;
+        }
+        String d = "[";
+        for (int i = 0; i < a.length; i ++){
+            d = d + a[i] + " ";
+        }
+        System.out.println(d + "]");
     }
         // - sort the array by converting it into a heap then removing the largest value n-1 times. [ should be O(nlogn) ]
 
