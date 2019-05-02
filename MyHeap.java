77165ec865c12767ab966fdf7b06608d1a1e740f
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-05-01 23:14:19 -0400

    Clean version; done

--- MyHeap.java
+++ MyHeap.java
@@ -2,44 +2,29 @@ public class MyHeap{
     private static void pushDown(int[]data,int size,int index){
         boolean done = false;
         while (!done && size > 0 && index * 2 + 1 < size){
-            // if (index * 2 + 2 < size){
-            //     String t = "[";
-            //     for (int i = 0; i < size; i ++){
-            //         t = t + data[i] + " ";
-            //     }
-            //     System.out.println(t + "]");
-            // }
-            // System.out.println(data[index] + "kcdovsdono");
-
-            // System.out.println("lljkdkahDKHSDKHA" + index + "   " + size);
-            //     System.out.println("okkokookkookokok");
                 if (index * 2 + 2 >= size ){
-                        // System.out.println("12345678543234567876543k");
                     if (data[index] >= data[index * 2 + 1]){
-                        // System.out.println("cry");
                         done = true;
-                        // System.out.println("huh");
                     }
                     else{
-                    // System.out.println("hold up");
-                    //swap
-                    int hold = data [index];
-                    data [index] = data [index * 2 + 1];
-                    data [index * 2 + 1] = hold;
-                    index = index * 2 + 1;
-                }
-                }
-                else if (data[index] > data[index * 2 + 1] && data[index] > data[index * 2 + 2]){
-                    // System.out.println("pip");
-                    done = true;
-                }
-                else if (data[index * 2 + 1] > data[index * 2 + 2]){
-
+                        //swap
                         int hold = data [index];
                         data [index] = data [index * 2 + 1];
                         data [index * 2 + 1] = hold;
                         index = index * 2 + 1;
                     }
+                }
+                else if (data[index] > data[index * 2 + 1] && data[index] > data[index * 2 + 2]){
+                    //if parent is greater than both children no need to continue
+                    done = true;
+                }
+                else if (data[index * 2 + 1] > data[index * 2 + 2]){
+                        // finds the bigger of the two to switch with the parent
+                        int hold = data [index];
+                        data [index] = data [index * 2 + 1];
+                        data [index * 2 + 1] = hold;
+                        index = index * 2 + 1;
+                }
                 else {
                         //swap
                         int hold = data [index];
@@ -50,8 +35,6 @@ public class MyHeap{
                     //set new index to fully bubble down
 
         }
-        // System.out.println("size  " + size + "         index"  + index);
-        // System.out.println("loolololololo");
     }
          // - size  is the number of elements in the data array.
          // - push the element at index i downward into the correct position. This will swap with the larger of the child nodes provided thatchild is larger. This stops when a leaf is reached, or neither child is larger. [ should be O(logn) ]
@@ -59,16 +42,18 @@ public class MyHeap{
          // - precondition: size is between 0 and data.length-1 inclusive.
 
     private static void pushUp(int[]data,int index){
-        int parent = data[index];
         boolean done = false;
         while (!done){
             if (index == 0){
+                // if at top, no need to conitinue
                 done = true;
             }
-            if (data[index] < data[(index + 1) / 2]){
+            else if (data[index] < data[(index + 1) / 2]){
+                // if it is less than its parent, no need to continue
                 done = true;
             }
             else{
+                // swap if it is greater than its parent
                 int hold = data [index];
                 data [index] = data [(index + 1) / 2];
                 data [(index + 1) / 2] = hold;
@@ -82,61 +67,32 @@ public class MyHeap{
 
     //We will discuss this today:
     public static void heapify(int[] a){
-        // System.out.println("kkkkkk");
         int index = a.length - 1;
         while (index >= 0){
-            // System.out.println(index);
             if(2 * index + 2 > a.length - 1){
-                // System.out.println("hip");
                 if (2 * index + 1 > a.length - 1){
                     index--;
                 }
             }
-                    // System.out.println("jkjkj");
-                    // String l = "[";
-                    // for (int i = 0; i < a.length; i ++){
-                    //     l = l + a[i] + " ";
-                    // }
-                    // System.out.println(l + "]");
-                    pushDown(a, a.length, index);
-                    // l = "hypo [";
-                    // for (int i = 0; i < a.length; i ++){
-                    //     l = l + a[i] + " ";
-                    // }
-                    // System.out.println(l + "]");
-                    index --;
-            }
-            }
-
-
-
+            // goes from bottom of the list to top to see if all the parents and children follow heap rules
+            pushDown(a, a.length, index);
+            // if not fix them
+            index --;
+        }
+    }
         // - convert the array into a valid heap. [ should be O(n) ]
-
     public static void heapsort(int[] a){
         heapify(a);
         int size = a.length;
         while (size > 0){
-            // System.out.println("help" + size);
+            // Switching removing one with the last element and then pucshing down that element
             int hold = a[0];
             a[0] = a[size - 1];
             a[size - 1] = hold;
-            // System.out.println("hhhh" + size);
-            // String t = "[";
-            // for (int i = 0; i < a.length; i ++){
-            //     t = t + a[i] + " ";
-            // }
-            // System.out.println(t + "]");
             pushDown(a, size - 1, 0);
+            // shrinking size to pretend the value just switched from the top, is no longer ther
             size --;
         }
-        // String d = "[";
-        // for (int i = 0; i < a.length; i ++){
-        //     d = d + a[i] + " ";
-        // }
-        // System.out.println(d + "]");
     }
         // - sort the array by converting it into a heap then removing the largest value n-1 times. [ should be O(nlogn) ]
-
-
-
 }
